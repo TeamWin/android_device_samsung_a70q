09a75f8698243ffed23deedc9025975b366b863a
Haky86 <hakamelassouad@gmail.com>
2020-11-23 23:12:36 +0100

    a70q: Drop TARGET_USERIMAGES_USE_F2FS
    * we don't support F2FS on this device anyways.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,6 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 
 # File systems
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 
 # Workaround for error copying vendor files to recovery ramdisk
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
